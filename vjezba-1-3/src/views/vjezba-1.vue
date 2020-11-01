1f548105dd3b9bc5634a2ee38007de6577ae988b
MatijaNovosel <mnovosel5@gmail.com>
2020-11-01 20:58:56 +0100

    Changes

--- vjezba-1-3/src/views/vjezba-1.vue
+++ vjezba-1-3/src/views/vjezba-1.vue
@@ -1,7 +1,27 @@
 <template>
-  <div class="text-center">
-    <span style="font-size: 20px" class="badge badge-pill badge-primary"
-      >Vježba 1</span
-    >
+  <div class="row text-center no-gutters">
+    <div class="col-12">
+      <span style="font-size: 20px" class="badge badge-pill badge-primary"
+        >Vježba 1</span
+      >
+    </div>
+    <div class="col-12 mt-5">
+      <img :src="require('../assets/elevator-diagram.png')" />
+    </div>
   </div>
 </template>
+
+<script lang="ts">
+import { defineComponent, onMounted } from "vue";
+
+export default defineComponent({
+  name: "vjezba-1",
+  setup() {
+    onMounted(() => {
+      //
+    });
+
+    return {};
+  }
+});
+</script>
