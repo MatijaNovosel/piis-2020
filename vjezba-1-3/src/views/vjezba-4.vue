d88905d795b13fe584f1de2b1c57e05d72ec9181
MatijaNovosel <mnovosel5@gmail.com>
2020-11-04 17:30:48 +0100

    Changes

--- vjezba-1-3/src/views/vjezba-4.vue
+++ vjezba-1-3/src/views/vjezba-4.vue
@@ -1,8 +1,10 @@
 <template>
-  <div class="row text-center no-gutters">
-    <div class="col-12 mt-5">
-      <img :src="require('@/assets/elevator/elevator-diagram-empty.png')" />
-    </div>
+  <div class="row text-center no-gutters mx-4 justify-content-center">
+    <group-box title="Dijagram">
+      <div class="col-12 mt-5">
+        <img :src="require('@/assets/elevator/elevator-diagram-empty.png')" />
+      </div>
+    </group-box>
   </div>
 </template>
 
